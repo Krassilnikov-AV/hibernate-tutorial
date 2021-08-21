d77d89c5cf3dfadd22f49420464f267c2a07ddd4
user <kavkras83@mail.ru>
2021-08-22 00:06:23 +0300

    refactoring: start project

---
+++ src/main/java/ru/hibermate/dao/DAO.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 Tander, All Rights Reserved.
+ */
+
+package ru.hibermate.dao;
+
+/**
+ * Класс DAO
+ */
+public class DAO {
+}
---
+++ src/main/java/ru/hibermate/model/Engine.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021 Tander, All Rights Reserved.
+ */
+
+package ru.hibermate.model;
+
+/**
+ * Класс Engine
+ */
+public class Engine {
+
+	private String model;
+	private int power;
+}
